4aa3a6147f6f62fe84c7bacf70bbaaf5669a03d7
razi24 <razdayan87@gmail.com>
2023-01-10 17:38:12 +0200

    razi

--- CSDjango/CSDjango/settings.py
+++ CSDjango/CSDjango/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,7 +126,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/'static',
+   
 ]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
