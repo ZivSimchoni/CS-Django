2048a0570855ac28cc1095977d3ea223472202e9
Aviv-Ash <82207941+Aviv-Ash@users.noreply.github.com>
2023-01-11 15:18:36 +0200

    Update settings.py

--- CSDjangoProj/CSDjangoProj/settings.py
+++ CSDjangoProj/CSDjangoProj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ulhu$+f#1mmr&ylr3tyl+olord=_2b2*cl6fbhm$ynl(q!+*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
