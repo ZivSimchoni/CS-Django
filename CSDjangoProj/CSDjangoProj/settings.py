f34357e4bf95e1b69900f7ad417bf1ba0a918a96
Aviv-Ash <82207941+Aviv-Ash@users.noreply.github.com>
2023-01-11 17:57:42 +0200

    Update settings.py

--- CSDjangoProj/CSDjangoProj/settings.py
+++ CSDjangoProj/CSDjangoProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ulhu$+f#1mmr&ylr3tyl+olord=_2b2*cl6fbhm$ynl(q!+*o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
